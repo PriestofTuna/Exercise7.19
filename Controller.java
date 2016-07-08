79b125e85cc3836307f8617b8d09416ab2e011d7
PriestofTuna <lyttekk@yahoo.com>
2016-07-08 00:27:14 -0400

    Initial commit

--- Controller.java
+++ Controller.java
@@ -2,15 +2,27 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import newGuI.GeneratedFields;
+import newGuI.GeneratedGUI;
 
 public class Controller {
-    public static int numTotal;
+    public static int numTotal = 0;
+    private static int[] numArray;
+
     @FXML private Button calcAmount;
     @FXML private TextField amount;
     @FXML
         public void calcAmountA(ActionEvent actionEvent) {
-        numTotal = Integer.parseInt(amount.getText());
+            numTotal = Integer.parseInt(amount.getText());
+        GeneratedFields generatedFields = new GeneratedFields(numTotal);
+
     }
-}
+
+
+    }
+
--- Main.java
+++ Main.java
@@ -2,17 +2,40 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Region;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
+import static javafx.geometry.Pos.*;
+
 public class Main extends Application {
+    Scene newScene;
+
+    public static Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+
+    private static Stage primaryStage;
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        this.primaryStage = primaryStage;
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
         primaryStage.setTitle("Number Sorter");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        newScene = (new Scene(root, bounds.getWidth(), bounds.getHeight()));
+        primaryStage.setScene(newScene);
+        primaryStage.setFullScreen(true);
         primaryStage.show();
     }
 
